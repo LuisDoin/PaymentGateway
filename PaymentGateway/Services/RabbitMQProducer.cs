9aa0bad87031169cd36c9da24cd03ddaa1e232b8
Luis Doin <luisdoin@gmail.com>
2022-12-18 00:02:58 -0300

    Add logging to RabbitMQProducer

--- PaymentGateway/Services/RabbitMQProducer.cs
+++ PaymentGateway/Services/RabbitMQProducer.cs
@@ -1,4 +1,5 @@
-﻿using RabbitMQ.Client;
+﻿using PaymentGateway.Controllers;
+using RabbitMQ.Client;
 using System.Text;
 
 namespace PaymentGateway.Services
@@ -6,20 +7,26 @@ namespace PaymentGateway.Services
     public class RabbitMQProducer : RabbitMQProducerBase
     {
         private readonly IModel _pendingTransactionsChannel;
+        private readonly ILogger<RabbitMQProducer> _logger;
 
-        public RabbitMQProducer(IConnection connection) : base(connection)
+        public RabbitMQProducer(IConnection connection, ILogger<RabbitMQProducer> logger) : base(connection)
         {
             _pendingTransactionsChannel = connection.CreateModel();
             _pendingTransactionsChannel.QueueDeclare("pending_transactions", durable: true, exclusive: false, autoDelete: false, arguments: null);
+            _logger = logger;
         }
 
         public override void Publish(string message)
         {
             if (string.IsNullOrWhiteSpace(message))
+            {
+                _logger.LogError("Message ill formed by the system");
                 throw new Exception("Message ill formed by the system");
+            }
 
             var body = Encoding.UTF8.GetBytes(message);
             _pendingTransactionsChannel.BasicPublish("", "pending_transactions", null, body);
+            _logger.LogInformation("Published message: " + message );
         }
     }
 }
